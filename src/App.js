fffffbc739f4360acc67f464df8a4701e61ed1ce
Viktor Pele <vikpelle@gmail.com>
2019-02-27 22:08:28 +0100

    Add grommet

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Grommet, Box, Button } from 'grommet';
+import theme from './theme';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <Grommet theme={theme}>
+        <Box align="center" background="neutral-2">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+          <Button
+            label="hello world"
+            primary
+            onClick={() => alert('hello, world')}
+          />
+        </Box>
+      </Grommet>
     );
   }
 }
